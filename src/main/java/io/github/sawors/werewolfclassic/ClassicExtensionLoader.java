b8820a55c01365ef57c31e8b6a29df3442137fbc
Sawors <yt.sawors@gmail.com>
2022-07-26 12:55:20 +0200

    imported roles

---
+++ src/main/java/io/github/sawors/werewolfclassic/ClassicExtensionLoader.java
@@ -0,0 +1,29 @@
+package io.github.sawors.werewolfclassic;
+
+import io.github.sawors.werewolfclassic.roles.cupid.Cupid;
+import io.github.sawors.werewolfclassic.roles.hunter.Hunter;
+import io.github.sawors.werewolfclassic.roles.littlegirl.LittleGirl;
+import io.github.sawors.werewolfclassic.roles.littlegirl.LittleGirlListenEvent;
+import io.github.sawors.werewolfclassic.roles.lover.Lover;
+import io.github.sawors.werewolfclassic.roles.seer.Seer;
+import io.github.sawors.werewolfclassic.roles.witch.Witch;
+import io.github.sawors.werewolfgame.extensionsloader.WerewolfExtension;
+
+public class ClassicExtensionLoader extends WerewolfExtension
+{
+    @Override
+    public void onLoad() {
+        registerNewRoles(
+                new Cupid(this),
+                new Hunter(this),
+                new LittleGirl(this),
+                new Lover(this),
+                new Seer(this),
+                new Witch(this)
+        );
+        
+        registerBackgroundEvents(
+                new LittleGirlListenEvent(this)
+        );
+    }
+}
---
+++ src/main/java/io/github/sawors/werewolfclassic/roles/witch/WitchPotionEvent.java
@@ -0,0 +1,31 @@
+package io.github.sawors.werewolfclassic.roles.witch;
+
+import io.github.sawors.werewolfgame.bundledextensions.classic.roles.witch.Witch;
+import io.github.sawors.werewolfgame.extensionsloader.WerewolfExtension;
+import io.github.sawors.werewolfgame.game.GameManager;
+import io.github.sawors.werewolfgame.game.GamePhase;
+import io.github.sawors.werewolfgame.game.events.GameEvent;
+import io.github.sawors.werewolfgame.game.events.RoleEvent;
+import io.github.sawors.werewolfgame.game.roles.PlayerRole;
+
+public class WitchPotionEvent extends GameEvent implements RoleEvent {
+    
+    public WitchPotionEvent(WerewolfExtension extension) {
+        super(extension);
+    }
+    
+    @Override
+    public void start(GameManager manager) {
+    
+    }
+    
+    @Override
+    public GamePhase getPhase() {
+        return GamePhase.NIGHT_POSTWOLVES;
+    }
+    
+    @Override
+    public PlayerRole getRole() {
+        return new Witch(extension);
+    }
+}
