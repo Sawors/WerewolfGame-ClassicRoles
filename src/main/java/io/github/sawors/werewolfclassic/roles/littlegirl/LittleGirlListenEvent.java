12a1fe057f15ff6e26ec58c3ff262228d1d7a235
Sawors <yt.sawors@gmail.com>
2022-08-05 22:01:52 +0200

    updated Witch events

--- src/main/java/io/github/sawors/werewolfclassic/roles/littlegirl/LittleGirlListenEvent.java
+++ src/main/java/io/github/sawors/werewolfclassic/roles/littlegirl/LittleGirlListenEvent.java
@@ -29,7 +29,12 @@ public class LittleGirlListenEvent extends BackgroundEvent implements RoleEvent 
         if(!event.getAuthor().isSystem() && !event.getAuthor().isBot() && this.manager != null && this.manager.getWolfChannel() != null && event.getChannel().getId().equals(this.manager.getWolfChannel().getId())){
             for(Map.Entry<TextChannel, TextRole> rolechan : manager.getRoleChannels().entrySet()){
                 if(rolechan.getValue().equals(getRole())){
-                    rolechan.getKey().sendMessage("**Loups** : *"+event.getMessage().getContentRaw()+"*").queue();
+                    String msg = event.getMessage().getContentRaw();
+                    String format = "*";
+                    if(msg.substring(0,4).equalsIgnoreCase("http")){
+                        format = "";
+                    }
+                    rolechan.getKey().sendMessage("**Loups** : "+format+msg+format).queue();
                 }
             }
         }
